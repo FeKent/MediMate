65a7b1605ebdbc1fc46928d8c294a8ace47b0357
FeKent <kentf022@gmail.com>
2024-05-14 23:24:10 +0100

    Update refill data type to LocalDate

--- app/src/main/java/com/fekent/medimate/data/Meds.kt
+++ app/src/main/java/com/fekent/medimate/data/Meds.kt
@@ -1,8 +1,10 @@
 package com.fekent.medimate.data
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.util.Date
+import java.time.LocalDate
 
 @Entity
 data class Meds(
@@ -10,13 +12,14 @@ data class Meds(
     val name: String,
     val dose: Int,
     val pillCount: Int,
-    val refill: Date
+    val refill: LocalDate
 )
 
+@RequiresApi(Build.VERSION_CODES.O)
 val meds = listOf(
-    Meds(0,  "Sertraline", 100, 28, Date(2024,1 ,8)),
-    Meds(1, "Sertraline", 100, 28, Date(2024,7 ,7)),
-    Meds(2, "Sertraline", 100, 28, Date(2024,7 ,7)),
-    Meds(3,"Sertraline", 100, 28, Date(2024,7 ,7)),
-    Meds(4, "Sertraline", 100, 28, Date(2024,7 ,7)),
+    Meds(0,  "Sertraline", 100, 28, LocalDate.of(2024, 6, 10)),
+    Meds(1, "Sertraline", 100, 28, LocalDate.of(2024, 4, 2)),
+    Meds(2, "Sertraline", 100, 28, LocalDate.of(2024, 7, 30)),
+    Meds(3,"Sertraline", 100, 28, LocalDate.of(2024, 11, 28)),
+    Meds(4, "Sertraline", 100, 28, LocalDate.of(2024, 8, 17)),
 )
