b13934f62200ff5cb082fe50df51cc93a2123343
FeKent <kentf022@gmail.com>
2024-05-31 14:39:52 +0100

    Set up Meds Database using Room

--- app/src/main/java/com/fekent/medimate/MainActivity.kt
+++ app/src/main/java/com/fekent/medimate/MainActivity.kt
@@ -1,26 +1,33 @@
 package com.fekent.medimate
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.room.Room
 import com.fekent.medimate.composables.AddMedsScreen
 import com.fekent.medimate.composables.CalendarScreen
 import com.fekent.medimate.composables.LandingScreen
 import com.fekent.medimate.composables.MedicationScreen
 import com.fekent.medimate.composables.SettingsScreen
+import com.fekent.medimate.data.MedsDatabase
 import com.fekent.medimate.ui.theme.MediMateTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -46,8 +53,17 @@ sealed class Screen(val route: String) {
 
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun MediMate(navController: NavHostController) {
+    val appContext = LocalContext.current
+    val database = remember {
+        Room.databaseBuilder(
+            appContext,
+            MedsDatabase::class.java,
+            "Meds Database"
+        ).build()
+    }
 
     NavHost(navController = navController, startDestination = Screen.Landing.route) {
         composable(Screen.Landing.route) {
