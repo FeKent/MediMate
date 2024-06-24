c97fb8fbe03118cd863a6494d2303ecc2b1a0f7e
FeKent <kentf022@gmail.com>
2024-06-24 20:04:49 +0100

    Add Textfields to Add Screen for user inputted Meds data

--- app/src/main/java/com/fekent/medimate/composables/AddMedsScreen.kt
+++ app/src/main/java/com/fekent/medimate/composables/AddMedsScreen.kt
@@ -1,6 +1,8 @@
 package com.fekent.medimate.composables
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -13,7 +15,12 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
@@ -23,11 +30,28 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.fekent.medimate.ui.theme.MediMateTheme
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 @Composable
 fun AddMedsScreen(back: () -> Unit) {
+    var name by remember { mutableStateOf("") }
+    var dose by remember { mutableStateOf("") }
+    var pillCount by remember { mutableStateOf("") }
+    var refill by remember { mutableStateOf("") }
+
+
     Column(modifier = Modifier.fillMaxSize()) {
         AddMedsBar{ back() }
+        Spacer(Modifier.size(24.dp))
+        TextField(value = name, onValueChange = {name = it})
+        TextField(value = dose, onValueChange = {dose = it})
+        TextField(value = pillCount, onValueChange = {pillCount = it})
+
+
+
+
+
     }
 }
 
