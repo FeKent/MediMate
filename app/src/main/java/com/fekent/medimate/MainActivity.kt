6ac0b6fd42676d29708c0e8b8c6715385fd31e5a
FeKent <kentf022@gmail.com>
2024-07-07 11:51:30 +0100

    Add Edit Screen to compose navigation

--- app/src/main/java/com/fekent/medimate/MainActivity.kt
+++ app/src/main/java/com/fekent/medimate/MainActivity.kt
@@ -10,22 +10,28 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import androidx.room.Room
 import com.fekent.medimate.composables.AddMedsScreen
 import com.fekent.medimate.composables.CalendarScreen
 import com.fekent.medimate.composables.LandingScreen
 import com.fekent.medimate.composables.MedicationScreen
 import com.fekent.medimate.composables.SettingsScreen
+import com.fekent.medimate.data.Meds
 import com.fekent.medimate.data.MedsDatabase
 import com.fekent.medimate.ui.theme.MediMateTheme
 import kotlinx.coroutines.launch
@@ -52,6 +58,7 @@ sealed class Screen(val route: String) {
     object Landing : Screen("Landing")
     object Settings : Screen("Settings")
     object AddMeds : Screen("AddMeds")
+    object EditMeds : Screen("EditMeds/{medId}")
     object Calendar : Screen("Calendar")
     object Medication : Screen("Medication")
 
@@ -101,7 +108,35 @@ fun MediMate(navController: NavHostController) {
                     database.medsDao().insertMeds(newMeds)
                 }
                 navController.popBackStack(Screen.Landing.route, inclusive = false)
-            })
+            }, medToEdit = null)
+        }
+        composable(
+            Screen.EditMeds.route,
+            arguments = listOf(navArgument("medId") { type = NavType.IntType })
+        ) { navBackStackEntry ->
+            val editScreenScope = rememberCoroutineScope()
+            val medId = navBackStackEntry.arguments?.getInt("medId")
+            if (medId != null) {
+                var med: Meds? by remember { mutableStateOf(null) }
+
+                LaunchedEffect(key1 = medId) {
+                    med = database.medsDao().getMeds(medId)
+                }
+                med?.let { editedMed ->
+                    AddMedsScreen(back = {
+                        navController.popBackStack(
+                            Screen.Landing.route,
+                            inclusive = false
+                        )
+                    }, onMedEntered = { newMeds ->
+                        editScreenScope.launch {
+                            database.medsDao().editMeds(newMeds)
+                        }
+                    }, medToEdit = editedMed)
+                }
+            }
+
+
         }
         composable(Screen.Calendar.route) {
             CalendarScreen(back = {
